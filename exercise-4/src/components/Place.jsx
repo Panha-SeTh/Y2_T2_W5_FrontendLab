c182a5021c67e01223c0f063d6124c88b2df9073
PanhaSeTh-Unknown <panhaseth.sophat@student.cadt.edu.kh>
2026-02-12 14:58:46 +0700

    Exercise5 done

--- exercise-4/src/components/Place.jsx
+++ exercise-4/src/components/Place.jsx
@@ -1,6 +1,6 @@
 export default function Place({ place }) {
   return (
-    <li className="place-item">
+    <li key={place.id} className="place-item">
       <button type="button">
         <img src={place.image.src} alt={place.image.alt} />
         <h3>{place.title}</h3>
